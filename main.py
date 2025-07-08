c17919db4be206aab67080084967dbbc53b677ab
Shahryar Salmaan <120364854+shahryar-salmaan@users.noreply.github.com>
2025-07-09 01:31:15 +0600

    Handle command and Task description
    It can now detect the command and also process the description.

--- main.py
+++ main.py
@@ -14,5 +14,27 @@ for line in WELCOME_MSG.splitlines():
 print("".center(width, "-"))
 
 while True:
-  user_input: str = input("Say hello > ")
-  print(user_input)
+  user_input: str = input("task-cli > ")
+  input_as_list = user_input.split(" ")
+  print(input_as_list)
+  command = input_as_list[0]
+  
+  print(command)
+  
+  if command == "add":
+    task_description_as_list = input_as_list[1:]
+    task_description = " ".join(task_description_as_list)
+    
+    if task_description.startswith('"') and task_description.endswith('"'):
+         task_description = task_description[1:-1]
+         
+    print(task_description)
+    
+    id = None
+    status = None
+    
+    task_dict = { "id": id,
+                  "status": status,
+                  "description": task_description }
+                  
+    print(task_dict)
