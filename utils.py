8ce287e56e3530b5762250448ffd2e642c123b6d
Shahryar Salmaan <120364854+shahryar-salmaan@users.noreply.github.com>
2025-07-13 04:33:47 +0600

    Add get_description

--- utils.py
+++ utils.py
@@ -11,3 +11,13 @@ def show_banner(width: int = 40) -> None:
     print(line.center(width, " ")) # Print lines with " " padding
     
   print("".center(width, "-")) # e.g. -----------
+  
+  
+def get_description(input_as_list):
+      task_description_as_list = input_as_list[1:] # Exclude the command (which is at index 0) and keep everything
+      task_description = " ".join(task_description_as_list) # Join the whole list and make a string
+      
+      if task_description.startswith('"') and task_description.endswith('"'): # Check if the description already has quotations
+           task_description = task_description[1:-1] # if it already has quotaions marks, it removes it so that we can store it without quotes inside a string
+      
+      return task_description
