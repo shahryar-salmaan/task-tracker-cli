885a06971776f8d1ee6c66927799013b4e136412
Shahryar Salmaan <120364854+shahryar-salmaan@users.noreply.github.com>
2025-07-13 04:07:36 +0600

    Add main function and exit command

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import sys
 
 WELCOME_MSG: str = ( 
       "Welcome to Task Tracker CLI\n"
@@ -15,32 +16,32 @@ for line in WELCOME_MSG.splitlines(): # Make a list of welcome strings
 
 print("".center(width, "-")) # e.g. -----------
 
-while True:
-  user_input: str = input("task-cli > ")
-  input_as_list = user_input.split(" ")
-  print(input_as_list)
-  command = input_as_list[0]
-  
-  print("The command is: ", command)
-  
-  if command == "add":
-    task_description_as_list = input_as_list[1:] # Exclude the command (which is at index 0) and keep everything
-    task_description = " ".join(task_description_as_list) # Join the whole list and make a string
+def add(input_as_list):
+      task_description_as_list = input_as_list[1:] # Exclude the command (which is at index 0) and keep everything
+      task_description = " ".join(task_description_as_list) # Join the whole list and make a string
+      
+      if task_description.startswith('"') and task_description.endswith('"'): # Check if the description already has quotations
+           task_description = task_description[1:-1] # if it already has quotaions marks, it removes it so that we can store it without quotes inside a string
+           
+      print("Task Description: ", task_description)
+
+def main():
+  while True:
+    user_input: str = input("task-cli > ")
+    input_as_list = user_input.split(" ")
+    print(input_as_list)
+    command = input_as_list[0]
     
-    if task_description.startswith('"') and task_description.endswith('"'): # Check if the description already has quotations
-         task_description = task_description[1:-1] # if it already has quotaions marks, it removes it so that we can store it without quotes inside a string
-         
-    print("Task Description: ", task_description)
+    print("The command is: ", command)
     
-    id = None
-    status = None
-    
-    task_dict = { "id": id,
-                  "status": status,
-                  "description": task_description }
-                  
-    print("Task Dictionary: ", task_dict)
-    
-    with open("tasks.json", "w") as f:
-      json.dump(task_dict, f, indent=4)
-    
+    if command in ["exit", "0"]:
+      print("Goodbye!")
+      sys.exit()
+    elif command == "add":  
+      add(input_as_list)
+        
+      with open("tasks.json", "r") as f:
+        content = json.load(f)
+        print("Content:", content)
+
+main()
