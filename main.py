a36d0e6b81dfb19f7f3f6a6075db09735cc82c4d
Shahryar Salmaan <120364854+shahryar-salmaan@users.noreply.github.com>
2025-07-12 23:59:37 +0600

    Add comments for me

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ WELCOME_MSG: str = (
       "Type 'help' to see all commands"
 )
 
-width: int = 40
+width: int = 40 # Width for padding
 
-print("".center(width, "-"))
+print("".center(width, "-")) # e.g. -----------
 
-for line in WELCOME_MSG.splitlines():
+for line in WELCOME_MSG.splitlines(): # Make a list of welcome strings
 
-  print(line.center(width, " "))
+  print(line.center(width, " ")) # Print lines with " " padding
 
-print("".center(width, "-"))
+print("".center(width, "-")) # e.g. -----------
 
 while True:
   user_input: str = input("task-cli > ")
@@ -19,16 +19,16 @@ while True:
   print(input_as_list)
   command = input_as_list[0]
   
-  print(command)
+  print("The command is: ", command)
   
   if command == "add":
-    task_description_as_list = input_as_list[1:]
-    task_description = " ".join(task_description_as_list)
+    task_description_as_list = input_as_list[1:] # Exclude the command (which is at index 0) and keep everything
+    task_description = " ".join(task_description_as_list) # Join the whole list and make a string
     
-    if task_description.startswith('"') and task_description.endswith('"'):
-         task_description = task_description[1:-1]
+    if task_description.startswith('"') and task_description.endswith('"'): # Check if the description already has quotations
+         task_description = task_description[1:-1] # if it already has quotaions marks, it removes it so that we can store it without quotes inside a string
          
-    print(task_description)
+    print("Task Description: ", task_description)
     
     id = None
     status = None
@@ -37,4 +37,4 @@ while True:
                   "status": status,
                   "description": task_description }
                   
-    print(task_dict)
+    print("Task Dictionary: ", task_dict)
